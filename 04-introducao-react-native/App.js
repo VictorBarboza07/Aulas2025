4dc4a8b3bf3e928056829a25f77d0c1cb4e32547
VictorBarboza07 <victor.barboza@iesb.edu>
2025-03-19 20:41:20 -0300

    alterações

--- 04-introducao-react-native/App.js
+++ 04-introducao-react-native/App.js
@@ -1,6 +1,6 @@
 //Imports
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, Image } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 //Componente Principal
 //Ele deve retorna o que será renderizado na tela (Template feito com JSX)
@@ -15,8 +15,9 @@ function alerta() {
 
 
 
-//Retorno com JSX 
+//Retorno com JSX     
   return (
+    <ScrollView>
     <View style={styles.container}>
       {/*Comentario dentro do Template JSX */}
       {/* Codigo javascript */}
@@ -24,10 +25,19 @@ function alerta() {
       <Text>{2 + 2}</Text>
       <Text>{nome}</Text>
       <Text>App do Victor!</Text>
-      <Button title='alerta' onPress={alerta}> </Button>
+
+      <Button title='Enviar' onPress={alerta}> </Button>
 
       <Image
         source={{ uri: "https://media1.tenor.com/m/o8EC-5TTaogAAAAd/macaco-olhando.gif"}} 
+        style={{
+          height: 300,
+          width: 300  
+        }}
+        />
+
+      <Image
+        source={require('./Imagens/image.png')} 
         style={{
           height: 300,
           width: 300
@@ -35,14 +45,16 @@ function alerta() {
         />
 
     </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
